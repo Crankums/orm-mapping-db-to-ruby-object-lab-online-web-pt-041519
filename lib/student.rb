f69eb6768431586fe807300d7187f39a89d8aa14
Craig Donavin <crdonavin@gmail.com>
2019-07-02 03:37:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,18 @@ class Student
       self.new_from_db(row)
     end.first
   end
-    # find the student in the database given a name
-    # return a new instance of the Student class
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
+  end
 
   def save
     sql = <<-SQL
